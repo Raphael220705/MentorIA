b8bcc2c72426cc22361d53c77b7a4bb84c21a214
Raphael <penharaphael94@gmail.com>
2025-11-19 19:41:31 -0300

    link api_base_url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Configuração da API
-const API_BASE_URL = ' http://localhost:8000';
+const API_BASE_URL = ' https://backend2-26.onrender.com';
 
 // Elementos DOM
 const chatMessages = document.getElementById('chatMessages');
